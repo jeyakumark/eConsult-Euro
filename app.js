c2bf3bfa805c7d3767a4d0dae21d53af563d60b6
jeyakumark <powertec3@gmail.com>
2015-02-01 16:56:59 +0800

    test camera

--- app.js
+++ app.js
@@ -36288,35 +36288,34 @@ module.exports = ElDashboardProfilePic = (function(_super) {
         cursor: 'pointer'
       }
     });
-    upload = (function(_this) {
-      return function(imageURI, timestamp) {
-        var ft, options, serverURL;
-        serverURL = Conf.imageServerURL + '/upload';
-        ft = new FileTransfer();
-        options = new FileUploadOptions();
-        options.fileKey = "file";
-        options.fileName = "filename.jpg";
-        options.mimeType = "image/jpeg";
-        options.chunkedMode = false;
-        options.params = {
-          timestamp: timestamp,
-          client_id: Session.currentClient.Id
-        };
-        ft.upload(imageURI, serverURL, (function(response) {
-          var client, large_picture, original_picture, sized_picture, square_picture;
-          original_picture = JSON.parse(response.response)['original'];
-          sized_picture = JSON.parse(response.response)['sized'];
-          square_picture = JSON.parse(response.response)['square'];
-          large_picture = JSON.parse(response.response)['large'];
-          Session.currentClient.setProfilePicUploaded(original_picture, sized_picture, square_picture, large_picture);
-          client = Session.currentClient;
-          return this.picture.setContent(client.profilePic());
-        }), (function(error) {
-          alert("Upload failed");
-          return console.log(error);
-        }), options);
+    upload = function(imageURI, timestamp) {
+      var ft, options, serverURL;
+      serverURL = Conf.imageServerURL + '/upload';
+      ft = new FileTransfer();
+      options = new FileUploadOptions();
+      options.fileKey = "file";
+      options.fileName = "filename.jpg";
+      options.mimeType = "image/jpeg";
+      options.chunkedMode = false;
+      options.params = {
+        timestamp: timestamp,
+        client_id: Session.currentClient.Id
       };
-    })(this);
+      ft.upload(imageURI, serverURL, (function(response) {
+        var large_picture, original_picture, sized_picture, square_picture;
+        original_picture = JSON.parse(response.response)['original'];
+        sized_picture = JSON.parse(response.response)['sized'];
+        square_picture = JSON.parse(response.response)['square'];
+        large_picture = JSON.parse(response.response)['large'];
+        Session.currentClient.setProfilePicUploaded(original_picture, sized_picture, square_picture, large_picture);
+        return Dispatcher.emit('page_change', {
+          to: 'Dashboard'
+        });
+      }), (function(error) {
+        alert("Upload failed");
+        return console.log(error);
+      }), options);
+    };
     this.addButton.on('click', (function(_this) {
       return function() {
         var onFail, onSuccess;
