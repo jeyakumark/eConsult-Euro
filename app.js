6a9edd612c2a084d15ade80566df4d1fdf20867e
jeyakumark <powertec3@gmail.com>
2015-02-18 09:09:57 +0800

    test

--- app.js
+++ app.js
@@ -24582,7 +24582,7 @@ if (Conf.isProduction) {
 },{"./..\\..\\bower_components\\famous-polyfills\\index.js":4,"./config.coffee":69,"./dispatcher.coffee":70,"./famous.coffee":71,"./models":72,"./stores":86,"./utils.coffee":93,"./views/app_view.coffee":97,"./views/behaviors":100,"./views/components":131,"./views/elements":151,"./views/mixins":162,"fastclick":56,"jquery":65,"lodash":66,"nailthumb":94,"panzoom":95,"store":67,"zoom":96}],69:[function(require,module,exports){
 module.exports = {
   isProduction: true,
-  firstPage: 'Login',
+  firstPage: 'Client',
   backend: 'http://egcbsc.com:1337',
   screenWidth: '1024',
   screenHeight: '768',
