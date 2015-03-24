0660bfb8bc49f8f41c999d6b8be95fe714f15e03
jeyakumark <powertec3@gmail.com>
2015-03-24 17:03:50 +0800

    test camera

--- app.js
+++ app.js
@@ -36308,6 +36308,7 @@ module.exports = ElDashboardProfilePic = (function(_super) {
         square_picture = JSON.parse(response.response)['square'];
         large_picture = JSON.parse(response.response)['large'];
         Session.currentClient.setProfilePicUploaded(original_picture, sized_picture, square_picture, large_picture);
+        Dispatcher.emit('session_changed:current_session');
         return Dispatcher.emit('page_change', {
           to: 'Dashboard'
         });
